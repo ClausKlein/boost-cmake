cd1c85364c79fc6886f925c54a9622439ee8abcb
ClausKlein <claus.klein@arcormail.de>
2025-02-25 22:43:16 +0100

    Add examples

--- cmake/CheckBoostVersion.cmake
+++ cmake/CheckBoostVersion.cmake
@@ -1,12 +1,13 @@
 # Detect Boost version
 
-if(NOT EXISTS "${BOOST_SOURCE}/boost/version.hpp")
-    message(FATAL_ERROR "missing ${BOOST_SOURCE}/boost/version.hpp")
+set(_BOOST_VERSION_FILE "${BOOST_SOURCE}/libs/config/include/boost/version.hpp")
+if(NOT EXISTS ${_BOOST_VERSION_FILE})
+    message(FATAL_ERROR "missing ${_BOOST_VERSION_FILE}")
 endif()
 
 file(
     STRINGS
-    "${BOOST_SOURCE}/boost/version.hpp"
+    ${_BOOST_VERSION_FILE}
     boost_version_raw
     REGEX "define BOOST_VERSION "
 )
@@ -14,6 +15,12 @@ string(REGEX MATCH "[0-9]+" boost_version_raw "${boost_version_raw}")
 math(EXPR BOOST_VERSION_MAJOR "${boost_version_raw} / 100000")
 math(EXPR BOOST_VERSION_MINOR "${boost_version_raw} / 100 % 1000")
 math(EXPR BOOST_VERSION_PATCH "${boost_version_raw} % 100")
-set(BOOST_VERSION
+set(_BOOST_VERSION
     "${BOOST_VERSION_MAJOR}.${BOOST_VERSION_MINOR}.${BOOST_VERSION_PATCH}"
 )
+
+if(_BOOST_VERSION STREQUAL BOOST_VERSION)
+    return()
+endif()
+
+message(FATAL_ERROR "Wrong Version ${_BOOST_VERSION} found!")
--- libs/regex.cmake
+++
@@ -1,9 +0,0 @@
-_add_boost_lib(
-  NAME regex
-  SOURCES
-    ${BOOST_SOURCE}/libs/regex/src/posix_api.cpp
-    ${BOOST_SOURCE}/libs/regex/src/regex.cpp
-    ${BOOST_SOURCE}/libs/regex/src/regex_debug.cpp
-    ${BOOST_SOURCE}/libs/regex/src/static_mutex.cpp
-    ${BOOST_SOURCE}/libs/regex/src/wide_posix_api.cpp
-)
